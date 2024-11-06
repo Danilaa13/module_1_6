d13e60ba21a5fd853f86e34fdfab09cc4dae5ab3
Danilaa13 <dk.prozak@gmail.com>
2024-11-06 12:39:13 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,4 +1,4 @@
 dz_count = 12
 time_count = 1.5
 name = "Python,"
-print(name, "всего задач:", dz_count, ",",  "затрачено часов:", time_count, ",", "среднее время выполнения", (time_count/dz_count), "часа.")
+print("Курс:", name, "всего задач:", dz_count, ",",  "затрачено часов:", time_count, ",", "среднее время выполнения", (time_count/dz_count), "часа.")
